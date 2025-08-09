ff98edd6d692427a8b32fa86d834aa875850c88f
Zattox <vladcsgoogurecHIK@MAIL.RU>
2025-08-09 23:10:37 +0300

    Add SQLAlchemy Linking table (User Segment Association)

---
+++ backend/app/associations_models/user_segment.py
@@ -0,0 +1,11 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.app.base import Base
+
+class UserSegmentAssociation(Base):
+    __tablename__ = "user_segment_association"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    segment_id: Mapped[int] = mapped_column(ForeignKey("segments.id"))
