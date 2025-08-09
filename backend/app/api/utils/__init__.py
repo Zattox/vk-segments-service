5d64395560ecc0b279587587b9b3981f8be7a97e
Zattox <vladcsgoogurecHIK@MAIL.RU>
2025-08-10 00:26:24 +0300

    Add distribute_segment

---
+++ backend/app/api/utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("distribute_segment_randomly",)
+
+from .distribute_segment import distribute_segment_randomly
---
+++ backend/app/api/utils/distribute_segment.py
@@ -0,0 +1,53 @@
+from sqlalchemy.orm import Session
+from typing import Optional
+import random
+
+from backend.app.models import TableSegment, TableUser
+from backend.app.schemes import DistributionResult
+
+from ..user import get_total_users_count, add_segment_to_user
+from ..segment import get_segment_by_name
+
+
+def distribute_segment_randomly(
+    session: Session,
+    segment_name: str,
+    percentage: float,
+) -> Optional[DistributionResult]:
+    """Distribute segment to random percentage of users"""
+    segment = get_segment_by_name(session, segment_name)
+    if not segment:
+        return None
+
+    total_users = get_total_users_count(session)
+    target_count = int(total_users * (percentage / 100))
+
+    # Get all users not currently in this segment
+    users_not_in_segment = (
+        session.query(TableUser)
+        .filter(~TableUser.segments.any(TableSegment.id == segment.id))
+        .all()
+    )
+
+    # Randomly select users to add to segment
+    if len(users_not_in_segment) >= target_count:
+        selected_users = random.sample(users_not_in_segment, target_count)
+    else:
+        selected_users = users_not_in_segment
+
+    # Add selected users to segment
+    assigned_count = 0
+    for user in selected_users:
+        if add_segment_to_user(session, user.id, segment.id):
+            assigned_count += 1
+
+    actual_percentage = (assigned_count / total_users) * 100 if total_users > 0 else 0
+
+    result = DistributionResult(
+        segment_name=segment_name,
+        total_users=total_users,
+        assigned_users=assigned_count,
+        percentage_achieved=actual_percentage,
+    )
+
+    return result
